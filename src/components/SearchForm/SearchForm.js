eb2f7e75acffac6857ceafc3a093ee5c3c69a392
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-03-11 18:59:36 +0100

    Delete function

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -9,23 +9,18 @@ import { useState } from 'react';
 const SearchForm = () => {
     const [searchLet, setSearchLet] = useState('');
     const dispatch = useDispatch();
-    //const searchString = useSelector(state => state.searchString);
-
 
     const handleSubmit = (e) => {
         e.preventDefault();
           dispatch({ type: 'UPDATE_SEARCHSTRING', payload: searchLet });
           setSearchLet('');
     }
-    const handleInputChange = (e) => {
-        setSearchLet(e.target.value);
-      }
 
     return (
         <form onSubmit={handleSubmit} className={styles.searchForm}>
             <TextInput placeholder="Search..." 
             value={searchLet} 
-            onChange={handleInputChange}
+            onChange={(e) => setSearchLet(e.target.value)}
             />
             <Button type="submit">
                 <span className="fa fa-search" />
