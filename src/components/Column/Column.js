3a920cc69320eba698ac68aae426eaf789f94093
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-03-12 15:50:04 +0100

    Add getFilteredCards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,17 +2,15 @@ import styles from './Column.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
 import { useSelector } from 'react-redux';
+import { getFilteredCards } from '../../redux/store';
 
 
 
 
 const Column = (props) => {
-  //const [searchString, setSearchString] = useState('');
 
-  const searchString = useSelector ((state) => state.searchString);
-  //const allCards = useSelector((state) => state.cards);
-  const cards = useSelector((state) => state.cards).filter(
-    (card) => card.columnId === props.id && card.title.toLowerCase().includes(searchString.toLowerCase()));
+
+    const cards = useSelector(state => getFilteredCards(state, props.id));
 
 
 
@@ -32,35 +30,6 @@ const Column = (props) => {
   );
 };
 
-/*const Column = (props) => {
-  const searchString = useSelector((state) => state.searchString);
-  const allCards = useSelector((state) => state.cards);
-
-  const cards = allCards.filter(
-    (card) => 
-    card.columnId === props.id && card.title.includes(searchString)
-  );
-
-  return (
-    <article className={styles.column}>
-      <h2 className={styles.title}>
-        <span className={styles.icon + ' fa fa-' + props.icon}></span>
-        {props.title}
-      </h2>
-      <ul className={styles.cards}>
-        {allCards
-          .filter((card) => card.columnId === props.id)
-          .map((card) => (
-            <Card key={card.id} title={card.title} />
-          ))}
-        {cards.map((card) => (
-          <Card key={card.id} title={card.title} />
-        ))}
-      </ul>
-      <CardForm columnId={props.id} />
-    </article>
-  );
-};*/
 
 
 export default Column;
