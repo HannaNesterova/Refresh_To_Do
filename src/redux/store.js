3e86add1f204ab7fbfc6742d165a1b9d4a02b1a5
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-03-12 17:08:29 +0100

    add strContains

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,17 @@
 import { createStore } from 'redux';
 import initialState from './initialState';
 import shortid from 'shortid';
+import strContains from '../utils/strContains';
 
 //selectors
-export const getFilteredCards = (state, columnId) => 
-state.cards.filter(card =>
-   card.columnId === columnId && card.title.toLowerCase().includes(state.searchString.toLowerCase()));
+export const getFilteredCards = ({ cards, searchString }, columnId) => cards
+  .filter(card => card.columnId === columnId && strContains(card.title, searchString));
+
+// export const getFilteredCards = ({ cards, searchString }, columnId) => cards
+//   .filter(card => card.columnId === columnId && card.title.toLowerCase().includes(searchString.toLowerCase()));
+// export const getFilteredCards = (state, columnId) => 
+// state.cards.filter(card =>
+//    card.columnId === columnId && card.title.toLowerCase().includes(state.searchString.toLowerCase()));
 
 const defaultState = {
   initialState: initialState,
---
+++ src/utils/strContains.js
@@ -0,0 +1,12 @@
+
+
+const strContains = (searchString, setSearchString ) => {
+    if(searchString.toLowerCase().includes(setSearchString.toLowerCase())) {
+        return true;
+    }
+    else{
+        return false
+    }
+}
+
+export default strContains;
